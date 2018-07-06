dacf6debff455200f5d700e24354bb99077f3203
Alpha Hinex <AlphaHinex@gmail.com>
2018-07-06 13:33:36 +0800

    Initial commit

---
+++ src/main/groovy/com/proper/oopstorm/secondlvcache/SecondLvCacheApplication.groovy
@@ -0,0 +1,12 @@
+package com.proper.oopstorm.secondlvcache
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SecondLvCacheApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SecondLvCacheApplication, args
+	}
+}
